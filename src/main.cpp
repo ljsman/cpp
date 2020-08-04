012632a3656dbd65948595e4233fff1a010d1df5
Jinshi <jli63@mail.depaul.edu>
2020-08-03 22:32:54 -0500

    before push to github

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,18 @@
 //  Created by Jinshi-Li on 7/24/20.
 //  Copyright © 2020 Jinshi-Li. All rights reserved.
 #include "test_space.hpp"
-#include "temp.h"
+
 #include <iostream>
 using namespace std;
  
-int main(int argc, const char * argv[])
+int main()
 {
     test_space ts;
     //ts.test_union_find();
     //ts.test_dp();
     ts.test_lcs();
-    temp tp;
+    
+    
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
---
+++ test_build/CMakeFiles/cpp.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/2y2ssn/CLionProjects/cpp/src/etc/temp.cpp" "/Users/2y2ssn/CLionProjects/cpp/test_build/CMakeFiles/cpp.dir/src/etc/temp.cpp.o"
+  "/Users/2y2ssn/CLionProjects/cpp/src/leetcode_problems/leetcode_cpp/dice_roll.cpp" "/Users/2y2ssn/CLionProjects/cpp/test_build/CMakeFiles/cpp.dir/src/leetcode_problems/leetcode_cpp/dice_roll.cpp.o"
+  "/Users/2y2ssn/CLionProjects/cpp/src/leetcode_problems/leetcode_cpp/solution.cpp" "/Users/2y2ssn/CLionProjects/cpp/test_build/CMakeFiles/cpp.dir/src/leetcode_problems/leetcode_cpp/solution.cpp.o"
+  "/Users/2y2ssn/CLionProjects/cpp/src/main.cpp" "/Users/2y2ssn/CLionProjects/cpp/test_build/CMakeFiles/cpp.dir/src/main.cpp.o"
+  "/Users/2y2ssn/CLionProjects/cpp/src/until/until_cpp/UnionFind.cpp" "/Users/2y2ssn/CLionProjects/cpp/test_build/CMakeFiles/cpp.dir/src/until/until_cpp/UnionFind.cpp.o"
+  "/Users/2y2ssn/CLionProjects/cpp/src/until/until_cpp/bfs.cpp" "/Users/2y2ssn/CLionProjects/cpp/test_build/CMakeFiles/cpp.dir/src/until/until_cpp/bfs.cpp.o"
+  "/Users/2y2ssn/CLionProjects/cpp/src/until/until_cpp/dp.cpp" "/Users/2y2ssn/CLionProjects/cpp/test_build/CMakeFiles/cpp.dir/src/until/until_cpp/dp.cpp.o"
+  "/Users/2y2ssn/CLionProjects/cpp/src/until/until_cpp/list.cpp" "/Users/2y2ssn/CLionProjects/cpp/test_build/CMakeFiles/cpp.dir/src/until/until_cpp/list.cpp.o"
+  "/Users/2y2ssn/CLionProjects/cpp/src/until/until_cpp/node.cpp" "/Users/2y2ssn/CLionProjects/cpp/test_build/CMakeFiles/cpp.dir/src/until/until_cpp/node.cpp.o"
+  "/Users/2y2ssn/CLionProjects/cpp/src/until/until_cpp/project_etc.cpp" "/Users/2y2ssn/CLionProjects/cpp/test_build/CMakeFiles/cpp.dir/src/until/until_cpp/project_etc.cpp.o"
+  "/Users/2y2ssn/CLionProjects/cpp/src/until/until_cpp/test_space.cpp" "/Users/2y2ssn/CLionProjects/cpp/test_build/CMakeFiles/cpp.dir/src/until/until_cpp/test_space.cpp.o"
+  "/Users/2y2ssn/CLionProjects/cpp/src/until/until_graph/graph.cpp" "/Users/2y2ssn/CLionProjects/cpp/test_build/CMakeFiles/cpp.dir/src/until/until_graph/graph.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  "../src"
+  "../src/leetcode_problems"
+  "../src/leetcode_problems/leetcode_cpp"
+  "../src/leetcode_problems/leetcode_header"
+  "../src/until"
+  "../src/until/until_cpp"
+  "../src/until/until_graph"
+  "../src/until/until_header"
+  "../src/etc"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
