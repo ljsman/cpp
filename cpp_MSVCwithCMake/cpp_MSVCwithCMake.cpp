17f24c93dc7995685505d903a02a5066b83e8c1e
Jinshi <jli63@mail.depaul.edu>
2020-09-16 17:45:43 -0500

    MSVC CMake in cpp_MSVCwithCMake

---
+++ cpp_MSVCwithCMake/cpp_MSVCwithCMake.cpp
@@ -0,0 +1,12 @@
+﻿// cpp_MSVCwithCMake.cpp : Defines the entry point for the application.
+//
+
+#include "cpp_MSVCwithCMake.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
